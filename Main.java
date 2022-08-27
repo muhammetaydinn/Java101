f609880637c5b4ef7282284094804974e89035a0
muhammetaydinn <muhammetaydin1704@gmail.com>
2022-08-28 02:16:36 +0300

    dersgecme 0-100 araligi

--- Main.java
+++ Main.java
@@ -2,20 +2,17 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-       // NotOrtalamasi.NotOrtalamasi();
-       //KDVTutari.KDVTutari();
-       //Hipotenus.Hipotenus();
-       //UcgendeAlan.UcgendeAlan();
-       //Taksimetre.Taksimetre();
-       //AlanCevreAlan.AlanCevreAlan();
-       //VKI.VKI();
-       //ManavKasa.ManavKasa();
-       //HesapMakinesi hm=new HesapMakinesi();
-       //ullaniciGirisi kg=new KullaniciGirisi();
-       //DersGecme dg=new DersGecme();
-
-
-
+        // NotOrtalamasi.NotOrtalamasi();
+        // KDVTutari.KDVTutari();
+        // Hipotenus.Hipotenus();
+        // UcgendeAlan.UcgendeAlan();
+        // Taksimetre.Taksimetre();
+        // AlanCevreAlan.AlanCevreAlan();
+        // VKI.VKI();
+        // ManavKasa.ManavKasa();
+        // HesapMakinesi hm=new HesapMakinesi();
+        // ullaniciGirisi kg=new KullaniciGirisi();
+        //DersGecme dg = new DersGecme();
 
     }
 }
