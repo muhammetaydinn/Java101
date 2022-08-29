2ce5be9664bd9360fdb7c06f74d0d7d7298d5a48
zaaydin1299 <zaaydin1299@gmail.com>
2022-08-29 23:20:01 +0300

    MukemmelSayi added

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) throws IOException {
+      //MukemmelSayi mS = new MukemmelSayi();
       // MinMax mm = new MinMax();
       // EbobEkok ebok = new EbobEkok();
       // ATM atm = new ATM();
---
+++ MukemmelSayi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MukemmelSayi {
+    public MukemmelSayi(){
+        int sayi,toplam=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Sayi giriniz: ");
+        sayi = sc.nextInt();
+        for(int i=1;i<sayi;i++){
+            if(sayi%i==0){
+                toplam+=i;
+            }
+        }
+        if(toplam==sayi){
+            System.out.println("Mukemmel sayi");
+        }
+        else{
+            System.out.println("Mukemmel sayi degil");
+        }
+
+
+    }
+}
