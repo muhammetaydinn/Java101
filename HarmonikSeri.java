85e06faabc4c0f4aee83532cc16b7e5c441646a5
zaaydin1299 <zaaydin1299@gmail.com>
2022-08-29 21:47:08 +0300

    Harmonikseri added

---
+++ HarmonikSeri.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HarmonikSeri {
+    public HarmonikSeri(){
+        //calculate the harmonic series
+        int n, i ;
+        double sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        n = sc.nextInt();
+        for (i = 1; i <= n; i++) {
+            sum += 1.0 / i;
+        }
+        System.out.println("The answer is: " + sum);
+        
+    }
+}
