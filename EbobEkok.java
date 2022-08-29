350ef901feb575caa158a97dfc13bbe8283906d6
zaaydin1299 <zaaydin1299@gmail.com>
2022-08-29 22:58:07 +0300

    ebobekok added

---
+++ EbobEkok.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class EbobEkok {
+    public EbobEkok() {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Birinci Sayi giriniz: ");
+        a = sc.nextInt();
+        System.out.print("Ikinci Sayi giriniz: ");
+        b = sc.nextInt();
+        System.out.println("Ebob: " + ebob(a, b));
+        System.out.println("Ekok: " + ekok(a, b));
+    }
+
+    public int ebob(int a, int b) {
+        if (a == b) {
+            return a;
+        } else if (a > b) {
+            return ebob(a - b, b);
+        } else {
+            return ebob(a, b - a);
+        }
+    }
+
+    public int ekok(int a, int b) {
+
+        return (a * b) / ebob(a, b);
+
+    }
+
+}
--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) throws IOException {
+      // EbobEkok ebok = new EbobEkok();
       // ATM atm = new ATM();
       // YildizElmas yildizElmas = new YildizElmas();
       // HarmonikSeri harmonikSeri = new HarmonikSeri();
