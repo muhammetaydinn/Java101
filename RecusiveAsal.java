27c2ead90445770decb3acdbe08bf196837b2917
zaaydin1299 <zaaydin1299@gmail.com>
2022-08-30 16:39:01 +0300

    recusive asal added

---
+++ RecusiveAsal.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+// says if a number is a prime number with recusive
+public class RecusiveAsal {
+    public RecusiveAsal(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayi Giriniz : ");
+        int number = scan.nextInt();
+        System.out.println(number +" sayisi " + (isPrime(number)==true ? "Asaldir." :"Asal Degil") );
+    }
+    
+    public boolean isPrime(int number) {
+        if (number == 1)
+            return false;
+        if (number == 2)
+            return true;
+        if (number % 2 == 0)
+            return false;
+        return isPrime(number, 3);
+    }
+    
+    public boolean isPrime(int number, int i) {
+        if (i * i > number)
+            return true;
+        if (number % i == 0)
+            return false;
+        return isPrime(number, i + 2);
+    }
+}
