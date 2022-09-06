450c8ca2f5d759170e9ea1e1e83f78b58cfb0b36
muhammetaydinn <muhammetaydin1704@gmail.com>
2022-09-06 05:41:02 +0300

    minesweeper added

---
+++ Bitirme/MayinTarlasi.java
@@ -0,0 +1,198 @@
+//Muhammet Aydın
+//Metin tabanlı mayın tarlası oyunu.
+
+package Bitirme;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MayinTarlasi {
+    public int[][] MayinYerlestir(int mayinSayisi, int satirSayisi, int sutunSayisi, int[][] matris) {
+        for (int i = 0; i < mayinSayisi; i++) {
+            int satir = 0;
+            int sutun = 0;
+            do {
+                satir = (int) (Math.random() * satirSayisi);
+                sutun = (int) (Math.random() * sutunSayisi);
+            } while (matris[satir][sutun] == 1);
+            matris[satir][sutun] = 1;
+        }
+        return matris;
+    }
+
+    public void print(int[][] matris, int[][] mayinKonumu) {
+        // prints the mineswerper
+        for (int i = 0; i < mayinKonumu.length; i++) {
+            for (int j = 0; j < mayinKonumu[i].length; j++) {
+                if (mayinKonumu[i][j] == 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(matris[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+    public void print2(int[][] mayinKonumu) {
+
+        for (int i = 0; i < mayinKonumu.length; i++) {
+            for (int j = 0; j < mayinKonumu[i].length; j++) {
+                if (mayinKonumu[i][j] == 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(mayinKonumu[i][j] + " ");
+
+                }
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    public int[][] mineCounter(int satirSayisi, int sutunSayisi, int[][] matris, int[][] mayinKonumu) {
+        int mS = 0;
+
+        // find how many mines are around
+        for (int i = 0; i < satirSayisi; i++) {
+            for (int j = 0; j < sutunSayisi; j++) {
+                if (mayinKonumu[i][j] == 0) {
+
+                    if (i - 1 >= 0 && j - 1 >= 0 && mayinKonumu[i - 1][j - 1] == 1) {
+                        mS++;
+                    }
+                    if (i > 0 && mayinKonumu[i - 1][j] == 1) {
+                        mS++;
+                    }
+                    if (i > 0 && j < sutunSayisi - 1 && mayinKonumu[i - 1][j + 1] == 1) {
+                        mS++;
+                    }
+                    if (j > 0 && mayinKonumu[i][j - 1] == 1) {
+                        mS++;
+                    }
+                    if (j < sutunSayisi - 1 && mayinKonumu[i][j + 1] == 1) {
+                        mS++;
+                    }
+                    if (i < satirSayisi - 1 && j > 0 && mayinKonumu[i + 1][j - 1] == 1) {
+                        mS++;
+                    }
+                    if (i < satirSayisi - 1 && mayinKonumu[i + 1][j] == 1) {
+                        mS++;
+                    }
+                    if (i < satirSayisi - 1 && j < sutunSayisi - 1 && mayinKonumu[i + 1][j + 1] == 1) {
+                        mS++;
+                    }
+                    matris[i][j] = mS;
+                    mS = 0;
+                }
+
+            }
+        }
+        return matris;
+
+    }
+
+    public void printMayinKonum(int[][] mayinKonumu) {
+        for (int i = 0; i < mayinKonumu.length; i++) {
+            for (int j = 0; j < mayinKonumu[i].length; j++) {
+                if (mayinKonumu[i][j] == 1) {
+                    System.out.println("[" + i + "][" + j + "]" + "mayın var");
+                }
+
+            }
+
+        }
+    }
+
+    public void copyTwoDimensionalArray(int[][] matris, int[][] mayinKonumu) {
+        for (int i = 0; i < matris.length; i++) {
+            for (int j = 0; j < matris[i].length; j++) {
+                mayinKonumu[i][j] = matris[i][j];
+            }
+        }
+    }
+
+    public void printSteppeds(boolean[][] steppeds, int satir, int sutun, int[][] matris, int[][] mayinKonumu) {
+        steppeds[satir][sutun] = true;
+        for (int i = 0; i < steppeds.length; i++) {
+            for (int j = 0; j < steppeds[i].length; j++) {
+                if (steppeds[i][j] == true) {
+                    if (mayinKonumu[i][j] == 1) {
+                        System.out.print("* ");
+
+                    } else {
+                        System.out.print(matris[i][j] + " ");
+                    }
+                } else {
+                    System.out.print("- ");
+
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    public MayinTarlasi() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kaç satır olacak?:\t");
+        int satirSayisi = sc.nextInt();
+
+        System.out.print("Kaç sütun olacak?:\t");
+        int sutunSayisi = sc.nextInt();
+
+        int[][] matris = new int[satirSayisi][sutunSayisi];
+        int[][] mayinKonumu = new int[satirSayisi][sutunSayisi];
+        boolean[][] steppeds = new boolean[satirSayisi][sutunSayisi];
+        for (boolean[] arr1 : steppeds)
+            Arrays.fill(arr1, false);
+
+        int mayinSayisi = satirSayisi * sutunSayisi / 4;
+        int stepCounter = 0;
+
+        System.out.println("Mayın sayısı: " + mayinSayisi);
+
+        matris = MayinYerlestir(mayinSayisi, satirSayisi, sutunSayisi, matris);
+        copyTwoDimensionalArray(matris, mayinKonumu);
+
+        matris = mineCounter(satirSayisi, sutunSayisi, matris, mayinKonumu);
+        // print(matris, mayinKonumu);
+
+        int satirNo, sutunNo;
+        boolean isSteppedonMine = false;
+
+        while (!isSteppedonMine) {
+            if(stepCounter+mayinSayisi==(satirSayisi*sutunSayisi)){
+                System.out.println("Tebrikler kazandınız!");
+                break;
+            }
+            System.out.print("Kacinci satiri seciyorsunuz :\t");
+            satirNo = sc.nextInt();
+            System.out.print("Kacinci sutunu seciyorsunuz :\t");
+            sutunNo = sc.nextInt();
+            try {
+                if (mayinKonumu[satirNo][sutunNo] == 1) {
+                    printSteppeds(steppeds, satirNo, sutunNo, matris, mayinKonumu);
+                    System.out.println("Mayına bastınız. Oyunu kaybettiniz.");
+                    System.out.println("Skorunuz: " + stepCounter);
+
+                    isSteppedonMine = true;
+                } else {
+                    System.out.println("Mayın yok");
+                    printSteppeds(steppeds, satirNo, sutunNo, matris, mayinKonumu);
+                    stepCounter++;
+
+                }
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Girdiginiz degerlerden birisi hatali");
+            }
+
+        }
+        sc.close();
+
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,81 +1,79 @@
 import java.io.IOException;
 import java.util.Arrays;
 
+import Bitirme.MayinTarlasi;
 import Diziler.*;
 import Diziler.ElemanFrekansi.ElemanFrekansi;
 import Diziler.PratikFolder.PratikFolder;
 import Diziler.Transpoz.Transpoz;
 import Siniflar.*;
 
-
 public class Main {
     public static void main(String[] args) throws NumberFormatException, IOException {
-        //Diziler
-        //new HarmonikOrtalama();
-        //new PratikMinMax();
-        // new  PratikArrayLetter();
-        //new PratikFolder();
-      // new ElemanFrekansi();
-      //new Transpoz();
-      //new SayiTahmin();
-      //new Palindrom();
-     
-
-    /* 
-        // Sınıflar
-        // new PratikObsMain();
-        // new BoksMain();
-        //new Employee("Kemal", 2000, 45, 1985);
-
-        //Donguler
-        // new ArmstrongNumber();
-        //new AsalSayi();
-        // new ATM();
-        // new EbobEkok();
-        // new FibonacciSerisi();
-        // new HarmonikSeri();
-        // new IkininUssu();
-        // new Kombinasyon();
-        // new MinMax();
-        // new MukemmelSayi();
-        // new TekSayiToplami();
-        // new UsluSayi();
-        // new YildizElmas();
-        // new YildizTersUcgen();
-
-        //Koşullu Ifadeler
-        // new ArtikYilHesaplama();
-        // new BurcProgrami();
-        // new CinZodyagi();
-        // new DersGecme();
-        // new HesapMakinesi();
-        // new KullaniciGirisi();
-        // new Sicaklik();
-        // new Siralama();
-        // new UcakBiletFiyat();
-
-        //Metotlar
-        // new PalindromSayi();
-        // new PratikHesapMak();
-        // new RecusiveAsal();
-        // new RecusiveDesen();
-        // new RecusiveFibonacci();
-        // new UsluRecusive();
-
-        //TemeLKavramlar
-         //new AlanCevreAlan();
-        // new Hipotenus();
-        // new KDVTutari();
-        // new ManavKasa();
-        // new NotOrtalamasi();
-        // new Taksimetre();
-        // new UceDordeTamBolunenler();
-        // new UcgendeAlan();
-        // new VKI();
-        */
+        // Diziler
+        // new HarmonikOrtalama();
+        // new PratikMinMax();
+        // new PratikArrayLetter();
+        // new PratikFolder();
+        // new ElemanFrekansi();
+        // new Transpoz();
+        // new SayiTahmin();
+        // new Palindrom();
+        // new MayinTarlasi();
         
+        /*
+         * // Sınıflar
+         * // new PratikObsMain();
+         * // new BoksMain();
+         * //new Employee("Kemal", 2000, 45, 1985);
+         * 
+         * //Donguler
+         * // new ArmstrongNumber();
+         * //new AsalSayi();
+         * // new ATM();
+         * // new EbobEkok();
+         * // new FibonacciSerisi();
+         * // new HarmonikSeri();
+         * // new IkininUssu();
+         * // new Kombinasyon();
+         * // new MinMax();
+         * // new MukemmelSayi();
+         * // new TekSayiToplami();
+         * // new UsluSayi();
+         * // new YildizElmas();
+         * // new YildizTersUcgen();
+         * 
+         * //Koşullu Ifadeler
+         * // new ArtikYilHesaplama();
+         * // new BurcProgrami();
+         * // new CinZodyagi();
+         * // new DersGecme();
+         * // new HesapMakinesi();
+         * // new KullaniciGirisi();
+         * // new Sicaklik();
+         * // new Siralama();
+         * // new UcakBiletFiyat();
+         * 
+         * //Metotlar
+         * // new PalindromSayi();
+         * // new PratikHesapMak();
+         * // new RecusiveAsal();
+         * // new RecusiveDesen();
+         * // new RecusiveFibonacci();
+         * // new UsluRecusive();
+         * 
+         * //TemeLKavramlar
+         * //new AlanCevreAlan();
+         * // new Hipotenus();
+         * // new KDVTutari();
+         * // new ManavKasa();
+         * // new NotOrtalamasi();
+         * // new Taksimetre();
+         * // new UceDordeTamBolunenler();
+         * // new UcgendeAlan();
+         * // new VKI();
+         */
 
     }
 
 }
-
